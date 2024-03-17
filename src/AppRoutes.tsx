4319e80b7043be31ec7c160b913a9094aa70a92f
soubj <sobujahmed124@gmail.com>
2024-03-17 14:26:26 +0600

    added layout and header

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,9 +1,10 @@
 import { Navigate, Route, Routes } from "react-router-dom";
+import Layout from "./layouts/Layout";
 
 const AppRoutes = () => {
     return (
         <Routes>
-            <Route path="/" element={<span>home</span>}/>
+            <Route path="/" element={<Layout><span>home</span></Layout>}/>
             <Route path="/user-profile" element={<span>user profile</span>}/>
             <Route path="*" element={<Navigate to="/"/>}/> //catch all routes
         </Routes>
