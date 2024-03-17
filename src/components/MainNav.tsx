9fd1fbf5958a93963e0329646856f1b755542a95
soubj <sobujahmed124@gmail.com>
2024-03-17 17:19:33 +0600

    added conditional login, logout and dropdown menu

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -1,16 +1,21 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from "./ui/button";
+import UserNameMenu from "./UserNameMenu";
 
 const MainNav = () => {
 
-    const {loginWithRedirect} = useAuth0();
+    const {loginWithRedirect,isAuthenticated} = useAuth0();
 
     return (
-        <Button variant="ghost" className="text-lg font-bold hover:text-orange-500 hover:bg-white"
-        onClick={()=>loginWithRedirect()}
-        >
-            Log In
-        </Button>
+        <div className="flex gap-2 items-center">
+            {isAuthenticated ? <UserNameMenu/>: 
+            <Button variant="ghost" className="text-lg font-bold hover:text-orange-500 hover:bg-white"
+            onClick={()=>loginWithRedirect()}
+            >
+                Log In
+            </Button>}
+        </div>
+        
     );
 };
 
---
+++ src/components/UserNameMenu.tsx
@@ -0,0 +1,34 @@
+import { CircleUserRound } from "lucide-react";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import { useAuth0 } from "@auth0/auth0-react";
+import { Link } from "react-router-dom";
+import { Button } from "./ui/button";
+
+const UserNameMenu = () => {
+    const {user,logout} = useAuth0();
+
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger className="flex items-center px-3 font-bold hover:text-orange-500 gap-2">
+                <CircleUserRound/>
+                {user?.name}
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <DropdownMenuItem>
+                <Link to="/user-profile" className="font-bold hover:text-orange-500 ">
+                    User Profile
+                </Link>
+                </DropdownMenuItem>
+                <DropdownMenuSeparator/>
+                <DropdownMenuItem>
+                    <Button className="w-full font-bold bg-orange-500"
+                    onClick={()=>logout()}>
+                        Log Out
+                    </Button>
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    );
+};
+
+export default UserNameMenu;
