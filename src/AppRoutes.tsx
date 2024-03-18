e5934eb71f7482832b178337858d32a81207c7c7
soubj <sobujahmed124@gmail.com>
2024-03-18 23:22:44 +0600

    create user and access token

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,11 +1,13 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./layouts/Layout";
 import HomePage from "./Pages/HomePage";
+import AuthCallbackPage from "./Pages/AuthCallbackPage";
 
 const AppRoutes = () => {
     return (
         <Routes>
             <Route path="/" element={<Layout><HomePage/></Layout>}/>
+            <Route path="/auth-callback" element={<AuthCallbackPage/>}/>
             <Route path="/user-profile" element={<span>user profile</span>}/>
             <Route path="*" element={<Navigate to="/"/>}/> //catch all routes
         </Routes>
--- src/api/UsersApi.tsx
+++ src/api/UsersApi.tsx
@@ -1,31 +1,43 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import { useMutation } from "@tanstack/react-query";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 type CreateUserRequest = {
-    auth0Id : string;
+    auth0Id: string;
     email: string;
 };
 
-export const useCreateUser = ()=>{
-    const createMyUserRequest = async(user: CreateUserRequest) =>{
-            const res = await fetch(`${API_BASE_URL}/api/v1/users/create-user`,{
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body:JSON.stringify(user)
-            });
+export const useCreateUser = () => {
+    const { getAccessTokenSilently } = useAuth0();
 
-            if(!res.ok){
-                throw new Error("Failed to create new user");
-            }
+    const createMyUserRequest = async (user: CreateUserRequest) => {
+        const accessToken = await getAccessTokenSilently();
+        const res = await fetch(`${API_BASE_URL}/api/v1/users/create-user`, {
+            method: "POST",
+            headers: {
+                "Authorization": `Bearer ${accessToken}`,
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(user),
+        });
+
+        if (!res.ok) {
+            throw new Error("Failed to create new user");
+        }
     };
 
-    const {mutateAsync: createUser, isPending, isError, isSuccess} = 
-    useMutation({mutationFn :createMyUserRequest});
+    const {
+        mutateAsync: createUser,
+        isPending,
+        isError,
+        isSuccess,
+    } = useMutation({ mutationFn: createMyUserRequest });
 
     return {
-        createUser, isPending, isError, isSuccess
-    }
-}
+        createUser,
+        isPending,
+        isError,
+        isSuccess,
+    };
+};
