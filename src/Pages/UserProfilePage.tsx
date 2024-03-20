07eccec77c330d755f5301b468a6df830d05d6c0
soubj <sobujahmed124@gmail.com>
2024-03-20 17:55:31 +0600

    added user profile page with form

---
+++ src/Pages/UserProfilePage.tsx
@@ -0,0 +1,9 @@
+import UserProfileForm from "@/forms/user-profile-from/UserProfileForm"
+
+const UserProfilePage = () => {
+    return (
+        <UserProfileForm/>
+    );
+};
+
+export default UserProfilePage;
--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -3,22 +3,21 @@ import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 
 type Props = {
-  children : React.ReactNode;
-}
+  children: React.ReactNode;
+  showHero?: boolean;
+};
 
-const Layout = ({children}: Props) => {
+const Layout = ({ children, showHero = false }: Props) => {
   return (
     <div className="flex flex-col min-h-screen">
-      <Header/>
-      <Hero/>
+      <Header />
+      {showHero && <Hero />}
 
-        <div className="container mx-auto flex-1 py-10">
-            {children}
-        </div>
+      <div className="container mx-auto flex-1 py-10">{children}</div>
 
-        <Footer/>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
 export default Layout;
